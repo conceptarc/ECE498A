bbdafa6ac43f78910cc146d184c9cdf14f99b49b
conceptarc <henryli1994@hotmail.com>
2016-06-12 12:44:15 -0400

    Fixed a bug
    - previous code was not correct

--- Pathing/A_Star.h
+++ Pathing/A_Star.h
@@ -2,6 +2,10 @@
 #define A_STAR_H
 
 #include <vector>
+#include <deque>
+#include <unordered_map>
+#include <unordered_set>
+#include <ctime>
 #include "Node.h"
 #include "Map.h"
 #define nullptr 0
@@ -10,124 +14,106 @@ using namespace std;
 
 class A_Star {
 private:
-	static bool Contains(vector<Node*> list, Node* target);
-	static vector<Node*> LowestCost(vector<vector<Node*>> &paths);
-	static vector<Node*> GetPath(vector<vector<Node*>> &paths, Node* &end, bool remove);
-	static vector<Node*> AppendToCopy(vector<Node*> existing, Node* target);
+	static Node* NextNode(unordered_map<Node*, float> &costList);
+	static deque<Node*> GetPath(unordered_map<Node*, Node*> &pathList, Node* &end);
 public:
 	static void FindPath(Map* map);
 };
 
-void A_Star::FindPath(Map* map) {
-	vector<Node*> openList;
-	vector<Node*> closedList;
-	vector<vector<Node*>> pathList; // list of parents for each node
-	float minDist = FLT_MAX;
+void A_Star::FindPath(Map* map2d) {
+	clock_t timer = clock(); // optional
 
-	Node* current = map->GetStart();
-	openList.push_back(current);
-	vector<Node*> initPath;
-	initPath.push_back(current);
-	pathList.push_back(initPath);
+	unordered_set<Node*> openList; // note that costList makes this redundant
+	unordered_set<Node*> closedList;
+	unordered_map<Node*, Node*> parentList;
+	unordered_map<Node*, float> costList;
+
+	Node* current = map2d->GetStart();
+	openList.insert(current);
+	parentList[current] = nullptr;
+	costList[current] = current->GetHeuristicDist();
 	int debugCounter = 0;
-	int limit = 2 * map->GetResolution()* map->GetResolution();
+	int limit = 2 * map2d->GetResolution()* map2d->GetResolution();
+	float deltaG = 100.0 / map2d->GetResolution();
 
-	while (debugCounter++ < limit) { // carefully
-		vector<Node*> lowest = LowestCost(pathList);
-		current = lowest.back();
-		int selectedIndex = -1;
-		for (int i = 0; i < openList.size(); i++) {
-			if (current == openList[i]) {
-				selectedIndex = i;
-				break;
-			}
-		}
-		if (selectedIndex < 0) {
-			cout << "WTF - it broke\r\n" << endl;
-			break;
-		}
-		// find lowest F cost and assign to current
-		openList.erase(openList.begin() + selectedIndex);
-		closedList.push_back(current);
-		if (current == map->GetGoal()) {
+	while (debugCounter++ < limit) {
+		
+		// find lowest F cost and assign that node as current
+		current = NextNode(costList);
+
+		openList.erase(current);
+		closedList.insert(current);
+		if (current == map2d->GetGoal()) {
+			double duration = (clock() - timer) / (double)CLOCKS_PER_SEC;
+
+
 			// print the final path and distance
-			cout << "done" << endl;
-			vector<Node*> finalPath = GetPath(pathList, current, false);
+			deque<Node*> finalPath = GetPath(parentList, current);
 			for each (Node* node in finalPath) {
 				node->SetPath(true);
 			}
-			map->Print();
+			for each (Node* node in closedList) {
+				node->SetVisited(true);
+			}
+			//map2d->Print();
+			cout << endl << map2d->GetResolution() << "*" << map2d->GetResolution() <<
+				" done:\r\ndistance = " << finalPath.size() - 1 <<
+				"\r\ntime = " << duration << "\r\nnodes visited = " << closedList.size() <<
+				"/" << map2d->GetResolution() * map2d->GetResolution() << endl;
 			break;
 		}
 
 		// note that paths contain the final node too
-		vector<Node*> currentPath = GetPath(pathList, current, true); // remove this one from the list too
 		vector<Node*> adjacent = current->GetAllAdjacent();
 		for (int i = 0; i < adjacent.size(); i++) {
-			if (Contains(closedList, adjacent[i])) continue;
-			if (adjacent[i]->IsOccupied()) {
-				closedList.push_back(adjacent[i]);
+			Node* node = adjacent[i];
+			if (node == nullptr || closedList.count(node) != 0) continue;
+			if (node->IsOccupied()) {
+				closedList.insert(node);
 				continue;
 			}
 			// if this is not in OPEN or if its a shorter path than the one in OPEN
 			// then add/replace in OPEN as needed
-
-			if (!Contains(openList, adjacent[i])) {
-				pathList.push_back(AppendToCopy(currentPath, adjacent[i]));
-				openList.push_back(adjacent[i]);
-			} else {
-				vector<Node*> existingPath = GetPath(pathList, adjacent[i], false);
-				vector<Node*> candidatePath = AppendToCopy(currentPath, adjacent[i]);
-				// note that paths contain the final node too
-				if (candidatePath.size() < existingPath.size()) {
-					existingPath = candidatePath;
-					// don't modify OPEN list
+			float temp = current->GetHeuristicDist();
+			float newPath = costList[current] - current->GetHeuristicDist() + deltaG + node->GetHeuristicDist();
+			if (openList.count(node) == 0) {
+				openList.insert(node);
+				costList[node] = newPath;
+				parentList[node] = current;
+			}
+			else {
+				// check if this path is shorter
+				float oldPath = costList[node];
+				if (newPath < oldPath) {
+					parentList[node] = current;
+					costList[node] = newPath;
 				}
 			}
 		}
+		costList.erase(current); // because it is not in the open list anymore
 	}
 }
 
-bool A_Star::Contains(vector<Node*> list, Node* target) {
-	for (int i = 0; i < list.size(); i++) {
-		if (list[i] == target) return true;
-	}
-	return false;
-}
-
-vector<Node*> A_Star::LowestCost(vector<vector<Node*>> &paths) {
-	float dist = FLT_MAX;
-	vector<Node*> cheapest;
-	for each (vector<Node*> path in paths)
-	{
-		if (path.size() + path.back()->GetHeuristicDist() < dist) {
-			dist = path.size() + path.back()->GetHeuristicDist();
-			cheapest = path;
+Node* A_Star::NextNode(unordered_map<Node*, float> &costList) {
+	float cost = FLT_MAX;
+	Node* selection = nullptr;
+	for (unordered_map<Node*, float>::iterator i = costList.begin(); i != costList.end(); i++) {
+		if (i->second < cost) {
+			cost = i->second;
+			selection = i->first;
 		}
 	}
-	return cheapest;
+	return selection;
 }
 
-vector<Node*> A_Star::GetPath(vector<vector<Node*>> &paths, Node* &end, bool remove) {
-	vector<Node*> path;
-	for (int i = 0; i < paths.size(); i++)
-	{
-		if (!paths[i].empty() && paths[i].back() == end) {
-			path = paths[i];
-			if (remove) paths.erase(paths.begin() + i);
-			break;
-		}
+deque<Node*> A_Star::GetPath(unordered_map<Node*, Node*> &parentList, Node* &end) {
+	deque<Node*> path;
+	while (end != nullptr) {
+		path.push_front(end);
+		end = parentList[end];
 	}
+
 	return path;
-}
-
-vector<Node*> A_Star::AppendToCopy(vector<Node*> existing, Node* target) {
-	vector<Node*> copy;
-	for each (Node* node in existing) {
-		copy.push_back(node);
-	}
-	copy.push_back(target);
-	return copy;
 }
 
 #endif

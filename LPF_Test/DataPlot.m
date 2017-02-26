3af1dd32d6d977ee49d041b2df54098a4acd85ad
Henry <l279li@uwaterloo.ca>
2017-02-26 14:10:24 -0500

    Added more data samples for the test

---
+++ LPF_Test/DataPlot.m
@@ -0,0 +1,7 @@
+csv = csvread('LPF_Test_Output_Only_Numbers_No_Aliasing.csv');
+w = 2*pi*csv(:,1);
+semilogx(w,csv(:,4))
+title('Experimental Frequency Magnitude Plot For Low Pass Filter');
+axis([-inf, inf, -inf, 5]);
+ylabel('Noise Power (Db)');
+xlabel('Noise Frequency (rad/s)');
--- LPF_Test/LPFTest/Tester.cpp
+++ LPF_Test/LPFTest/Tester.cpp
@@ -8,21 +8,22 @@
 using namespace std;
 
 int main() {
-	float SAMPLE_RATE = 30.3; // Hz
+	float SAMPLE_RATE = 30.3f; // Hz
 	int testSecondsLimit = 10; // seconds
 	int noiseFrequencyLimit = 30; // hertz
 
-	for (int noiseFreq = 0; noiseFreq < noiseFrequencyLimit + 1; noiseFreq++) {
+	printf("noise_f, input_avg_power, output_avg_power, Db\r\n");
+	for (float noiseFreq = 0; noiseFreq <= noiseFrequencyLimit + 0.1; noiseFreq += 0.1f) {
 		Buffer_Wrapper buffer = Buffer_Wrapper();
-		float avgX = 0;
-		float avgXb = 0;
+		float avgPowerX = 0;
+		float avgPowerXb = 0;
 
 		// only test X component - sufficient
 		for (int i = 0; i < testSecondsLimit * SAMPLE_RATE; i++) {
 			float t = i / ((float)SAMPLE_RATE);
 
 			// phaseOffset is to avoid having sampling time be some multiple of frequency
-			float xInput = (float)sin(2*PI*noiseFreq*t + phaseOffset);
+			float xInput = (float)sin(2 * PI*noiseFreq*t + phaseOffset);
 
 			int id = 111;
 			float xBuffer = 0;
@@ -32,15 +33,16 @@ int main() {
 
 			buffer.Update_Object(id, xInput, 0, t);
 			buffer.Get_Object_Properties(id, xBuffer, yBuffer, dxBuffer, dyBuffer);
-			avgX += abs(xInput);
-			avgXb += abs(xBuffer);
+			avgPowerX += xInput*xInput/2;
+			avgPowerXb += xBuffer*xBuffer/2;
 		}
-		avgX = avgX / (float)(testSecondsLimit * SAMPLE_RATE);
-		avgXb = avgXb / (float)(testSecondsLimit * SAMPLE_RATE);
-		float db = 20 * log10(avgXb / avgX);
+		avgPowerX = avgPowerX / (float)(testSecondsLimit * SAMPLE_RATE);
+		avgPowerXb = avgPowerXb / (float)(testSecondsLimit * SAMPLE_RATE);
+		float db = 20 * log10(avgPowerXb / avgPowerX);
 
 		// notice the aliasing effect after 15 Hz due to 30 Hz sampling rate
-		printf("noise_f = %2d, input_avg = %2.4f, output_avg = %2.4f, Db = %6.2f \r\n", noiseFreq, avgX, avgXb, db);
+		//printf("noise_f = %5.2f, input_avg = %2.4f, output_avg = %2.4f, Db = %6.2f \r\n", noiseFreq, avgX, avgXb, db);
+		printf("%.1f,%.4f,%.4f,%.4f\r\n", noiseFreq, avgPowerX, avgPowerXb, db);
 		//printf("\r\n");
 	}
 
